d5c008693808208bc3beb164d059d84cb21be151
jose <josele3985@hotmail.com>
2021-09-25 20:36:53 +0200

    Inicio proyecto
    Inicio proyecto->creacion del proyecto

---
+++ calculadora/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+const contenedor=`
+  width:40%;
+  margin:150px auto;
+`
+export default App;
